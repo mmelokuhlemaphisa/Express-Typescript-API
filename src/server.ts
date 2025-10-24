40a90a3e5fac899e0177d44acdbf0164ec4111fe
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-10-24 12:29:46 +0200

    feat:Done doing set up of custom middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
+import bodyParser from "body-parser";
 import express, { Express } from "express";
+import { loggerMiddleware } from "./middleware/logger";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(loggerMiddleware);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
